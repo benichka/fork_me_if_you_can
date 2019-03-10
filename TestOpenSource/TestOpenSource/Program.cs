4b95161d34a56db89692d71c4c797bc5ec98458a
Benoit Masson-Bedeau <benoit.bedeau@gmail.com>
2019-03-10 18:47:04 +0100

    Create a 'Hello World' project.

---
+++ TestOpenSource/TestOpenSource/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestOpenSource
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
